9ee65a90c4a027096ab3e6761865f1e8d5db3d7c
Margarita-Anisimova <margarita311@autorambler.ru>
2021-04-26 19:14:54 +0500

    flipping cards

--- WebApp/ClientApp/src/components/CheckCard.js
+++ WebApp/ClientApp/src/components/CheckCard.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import "./CheckCard.css"
 import { Link } from 'react-router-dom';
-import { NavLink } from 'reactstrap';
 
 export class CheckCard extends Component {
-
     constructor(props) {
         super(props);
-        this.state = { word: this.collection[0].En_name, id: 0, buttons: ["Я знаю", "Я не знаю"], pageState: this.firststate };
+
+        this.correct = this.correct.bind(this);
+        this.incorrect = this.incorrect.bind(this);
+
+        this.state = { id: 0, numberСorrectWords: 0 };
     }
-
-    dict = {
-        "Я знаю": { flag: true, way: ["Я так и думал", "Я ошибся"] },
-        "Я не знаю": { flag: true, way: ["Вспомнил", "Запомню"] },
-        "Я так и думал": { flag: false, param: 1 },
-        "Я ошибся": { flag: false, param: -1 },
-        "Вспомнил": { flag: false, param: -1 },
-        "Запомню": { flag: false, param: 0 },
-    };
-
-    numberСorrectWords = 0;
-
 
     collection = [
         {
@@ -35,75 +25,107 @@ export class CheckCard extends Component {
             countOfTrue: 3,
         },
         {
-            En_name: "cat",
+            En_name: "Сat",
             Ru_name: "Кошка",
             countOfTrue: 2,
         }
     ];
 
-    reRender(prop) {
-        if (prop === "Я так и думал") {
-            this.numberСorrectWords++;
-        }
-        let newButtons = this.dict[prop].way;
-        let newWord = this.collection[this.state.id].Ru_name;
-        let newId = this.state.id;
-        if (!this.dict[prop].flag) {
-            newButtons = ["Я знаю", "Я не знаю"];
-            newId = this.state.id + 1;
-            if (newId === this.collection.length) {
-                this.setState({ pageState: this.secondstate })
-                return;
-            }
-            newWord = this.collection[newId].En_name;
-            this.collection[this.state.id].countOfTrue = this.dict[prop].param
-                ? this.collection[this.state.id].countOfTrue + this.dict[prop].param
-                : 0;
-        }
-        this.setState({
-            id: newId,
-            word: newWord,
-            buttons: newButtons,
-        })
+    correct() {
+        this.setState((state) => {
+            state.id++;
+            state.numberСorrectWords++;
+            return state;
+        });
     }
 
-    renderButtons() {
-        let res = [];
-        for (const e of this.state.buttons) {
-            res.push(
-                <button className="all-button answer-button" onClick={() => this.reRender(e)}>{e}</button>
-            )
-        }
-        return <div className="answer">{res}</div>
+    incorrect() {
+        this.setState((state) => {
+            state.id++;
+            return state;
+        });
     }
 
 
-    firststate = () => {
-        let grad = `linear-gradient(to top, rgb(60, 62, 60) 10%, rgb(41, 41, 46)50%, rgb(60, 62, 60) 80%, green 100%)`;
-        return (
-            <div className="card" style={{ background: grad }}>
-                <p>{this.state.word}</p>
-                {this.renderButtons()}
-            </div>
-        );
-    }
-
-    secondstate = () => (
-        <div className="card result">
-            <p>МОЛОДЕЦ!!!</p>
-            {`Решено верно ${this.numberСorrectWords}/${this.collection.length}`}
-            <NavLink tag={Link} className="all-button back" style={{ width: '200px' }} to="/check">К другим наборам</NavLink>
-
-        </div>
-    );
-
+    // TODO: вообще прикольно было бы сделать анимацию передвижения карточек
     render() {
         return (
             <div className="card-box">
-                {this.state.pageState()}
+                {
+                    this.state.id !== this.collection.length ?
+                        <CommonCardTest key={this.state.id} english={this.collection[this.state.id].En_name} russian={this.collection[this.state.id].Ru_name} correct={this.correct} incorrect={this.incorrect} />
+                        :
+                        <Finish correct={this.state.numberСorrectWords} total={this.collection.length} />
+                }
             </div>
         );
     }
+}
 
+class CommonCardTest extends Component {
+    constructor(props) {
+        super(props);
+        this.hover = this.hover.bind(this);
+    }
+    changeButtons1() {
+        document.getElementById('b1').textContent = "Так и знал";
+        document.getElementById('b2').textContent = "Ошибся";
 
+        document.getElementById('flip').classList.toggle('hover');
+    }
+    changeButtons2() {
+        document.getElementById('b1').textContent = "Вспомнил";
+        document.getElementById('b2').textContent = "Запомню";
+
+        document.getElementById('flip').classList.toggle('hover');
+    }
+
+    hover() {
+        //document.getElementById('flip').classList.toggle('hover');
+    }
+
+    render() {
+        return (
+            <div id="flip" className="flip-container">
+                <div className="flipper">
+                    <div className="front">
+                        <div className="card">
+                            <p>{this.props.english}</p>
+                            <div className="answer">
+                                <a className="all-button answer-button" onClick={this.changeButtons1}>Знаю</a>
+                                <a className="all-button answer-button" onClick={this.changeButtons2}>Не знаю</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="back">
+                        <div className="card">
+                            <p>{this.props.russian}</p>
+                            <div className="answer">
+                                <a id="b1" className="all-button answer-button" onClick={() => { this.props.correct(); this.hover(); }} ></a>
+                                <a id="b2" className="all-button answer-button" onClick={() => { this.props.incorrect(); this.hover(); }}></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Finish extends Component {
+
+    render() {
+        return (
+            <div id="flip" className="flip-container">
+                <div className="front">
+                    <div className="card">
+                        <p>{this.props.correct} из {this.props.total}</p>
+                        <div className="answer">
+                            <Link className="all-button answer-button" to='/check'>К другим наборам</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
